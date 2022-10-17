824b17b0449824b8ced039c876d341a17d40dd0b
JeffreyHSE <Jeffrey.Ejeanye@hoffmann-group.com>
2022-10-17 18:01:47 +0200

    created index.js to post new data into the database when submit has been clicked.

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,38 @@
+let submit = document.getElementById("booking_shop_submit");
+
+submit.addEventListener("click", e => {
+    e.preventDefault();
+
+    let fName = document.getElementsByClassName("inputFirstName")[0].value;
+    let lName = document.getElementsByClassName('inputLastName')[0].value;
+    let date = document.getElementsByClassName('inputDate')[0].value;
+    let barber = document.getElementsByClassName('inputBarberChoice')[0].value;
+
+
+    if (fName.length === 0 || lName.length === 0 | date.length === 0 || barber.length === 0 ) {
+        window.alert("You missed something to fill in.")
+        return;
+    }
+
+    const data = { 'name':fName };
+
+    fetch('http://localhost:5000/bookings/add', {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+
+    console.log(fName);
+    console.log(lName);
+    console.log(date);
+    console.log(barber);
+});
