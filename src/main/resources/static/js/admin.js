64030ad2fd83bb45fd93c833650e283d5a86f5a2
JeffreyHSE <Jeffrey.Ejeanye@hoffmann-group.com>
2022-10-19 11:17:24 +0200

    created admin.js file and made it possible to display all new appointments in a table

---
+++ src/main/resources/static/js/admin.js
@@ -0,0 +1,37 @@
+let barber_table = document.createElement("table");
+
+let body = document.querySelector("body");
+
+async function make_async() {
+    let response = await fetch('http://localhost:5000/bookings/', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    let appointment_data =  await response.json();
+    console.log(appointment_data);
+
+        barber_table.innerHTML = `
+  <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Date</th>
+    <th>Barber</th>
+  </tr>`;
+
+ Object.values(appointment_data).forEach(e => {
+     let barber_data = document.createElement("tr");
+     barber_data.innerHTML =
+         `
+         <td>${e.firstName}</td>
+         <td>${e.lastName}</td>
+         <td>${e.date}</td>
+         <td>${e.barber}</td>`;
+    barber_table.appendChild(barber_data);
+ });
+
+    body.insertAdjacentElement("afterbegin", barber_table);
+}
+make_async();
